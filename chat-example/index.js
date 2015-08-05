cbffff4d4fb9647da3958f75bcd0ca48f8927b67
costasdevelop <costasdevelop@gmail.com>
2015-08-05 14:16:23 +0300

    the basic chat is working now

--- chat-example/index.js
+++ chat-example/index.js
@@ -27,6 +27,8 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
+    //Broadcast the message to everybody
+    io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
